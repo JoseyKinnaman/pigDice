5d08cc14566beb804f865582245cefa62ba45677
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-09 17:07:47 -0700

    update js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,79 @@
-// <!--Back End for Game-->
+// <!--Back End -->
 
+// Backend For Game
 function Game (player1, player2){
   this.player1 = player1;
   this.player2 = player2;
-  this.currentPlayer = true;
+  this.currentPlayer = "player1";
 };
 
+// Switch player Turn ==>
 Game.prototype.switchTurn = function () {
-  if (this.currentPlayer) {
-    this.currentPlayer = false
+  if(this.currentPlayer === "player1"){
+    this.currentPlayer = "player2"
   } else {
-    this.currentPlayer = true
+    this.currentPlayer = "player1"
   }
 }
 
-Game.prototype.currentPlayer = function(){
-  if (this.currentPlayer) {
-    return this.player1
-  } else {
-    return this.player2
-  }
-}
-//  Backend for Player//
+// Backend for Player//
 function Player() {
   this.roll = 0;
   this.score = 0; 
   this.total = 0;
 }
 
-Player.prototype.playerTurn = function () {
+// Roll button method  ==> outputs a random number 
+Game.prototype.playerTurn = function () {
   var newRoll = Math.floor(Math.random() * 6) + 1;
-  this.roll = newRoll;
+  // this.roll = newRoll;
+  // this[this.currentPlayer]
+  //who is playing? 
+
   if (newRoll === 1) {
-    this.roll = 0;
-    this.score = 0;
+    this[this.currentPlayer].roll = 0;
+    this[this.currentPlayer].score = 0;
     alert("You rolled 1! Score is 0. Your turn is over!")
-    game.switchTurn();
-    $("#p1").toggleClass("yellow-background");
-    $("#p2").toggleClass("yellow-background");
+    this.switchTurn();
+    // $("#p1").toggleClass("yellow-background");
+    // $("#p2").toggleClass("yellow-background");
   } else {
-    this.score += newRoll;
+    this[this.currentPlayer].score += newRoll;
   }
-  return this.score;
+  console.log("current score: ", this.player1.score)
+  console.log("current roll: ", this.player1.roll)
 }
 
-Player.prototype.totalDisplayed = function(totalCurrent, totalAdded){
-totalCurrent.text(this.score);
-totalAdded.text(this.total);
-}
-
-
-  Player.prototype.holdTurn = function (game) {
     
-    
-    
-    // var roll = newRoll
-    // if (roll != 1 ){
-    //   return this.score += roll;
-    // }else if (roll= 1) {
-    //   $("#hold").toggle();
-      
-    //   return this.score
-    // }
-  }
 
 // Ui-Front Logic
 // Creates our two players for the game.
+$(document).ready(function () {
 var player1 = new Player();
 var player2 = new Player();
 var game = new Game(player1, player2); 
 
-$(document).ready(function () {
-  $("#roll").click(function (event) {
-    event.preventDefault();
-    $("#diceman").toggle();
-    if (this.currentPlayer) { 
-       $("#display-dice").prepend("<li>" + player1.playerTurn() + "</li>");
-    } else {
-      $("#display-dice").prepend("<li>" + player2.playerTurn() + "</li>");
-    }
+// Hold button for players
+  $("#hold").click(function () {
+    
+    // var player = game.switchTurn();
+    $("#player-one-total").text(" " + player1.score);
+    $("#player-two-total").text(" " + player2.score);
+  });
+
+// Roll button for players 
+  $("#roll").click(function () {
+    var player = new Game(currentPlayer);
+    player.playerTurn();
+   $("#dice-box").text(" " + currentPlayer.roll);
+// $("#diceman").toggle();
+
+
+
+
   });
 });
 
-$(document).ready(function () {
-  $("#hold").click(function (event) {
-    event.preventDefault();
-    $("#player-one-total").text(" " + this.score);
- });
-}); 
 
 
 
@@ -96,15 +82,8 @@ $(document).ready(function () {
 
 
 
-// player.hold();
-    // $("#player-two-total").text(" " + player1.score);
+
 
     
-  
 
-// function attachContactListeners() {
-//   $("#dice-box").on("click", function(){
-//     showTotal(this.total)
-//   })
-// }
 
